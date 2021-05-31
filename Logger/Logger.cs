9397c3dafd707aee4b465770d39971d3dfcc4b3e
LetRuin <crot9000@yandex.ru>
2021-05-31 19:07:21 +0800

    Создан класс работы с Легированием в БД SQLite

---
+++ Logger/Logger.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Data;
+using Microsoft.Data.Sqlite;
+
+namespace Logger
+{
+    public class Logger
+    {
+        private const string nameBD = "Data Source=Log.sqlite";
+        private SqliteConnection bd;
+
+        public Logger()
+        {
+            try
+            {
+                bd = new SqliteConnection("nameBD");
+                bd.Open();
+                var CreateTable = "CREATE table IF NOT EXISTS Log (id integer PRIMARY KEY AUTOINCREMENT NOT NULL,time char(50) NOT NULL,type char(20) NOT NULL,message char(100) NOT NULL)";
+                SqlRequest(CreateTable);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("e");
+                throw new Exception($"{e}");
+            }
+        }
+
+        private void SqlRequest(string message)
+        {
+            using (SqliteCommand command = new SqliteCommand())
+            {
+                try
+                {
+                    command.Connection = bd;
+                    command.CommandText = "message";
+                    command.CommandType = CommandType.Text;
+                    command.ExecuteNonQuery();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("e");
+                    throw new Exception($"{e}");
+                }
+            }
+        }
+        public void CustomLog(string type,string message)
+        {
+            var sqlRequest = $"INSERT INTO Log (time,type,message) VALUES ('{DateTime.Now:G}', '{type}', '{message}');";
+            SqlRequest(sqlRequest);
+        }
+        public void TypeLog(LogType type,string message)
+        {
+            var sqlRequest = $"INSERT INTO Log (time,type,message) VALUES ('{DateTime.Now:G}', '{type.ToString()}', '{message}');";
+            SqlRequest(sqlRequest);
+        }
+
+        public void Info(string message)
+        {
+            TypeLog(LogType.Info, message);
+        }
+        public void Success(string message)
+        {
+            TypeLog(LogType.Success, message);
+        }
+        public void Warning(string message)
+        {
+            TypeLog(LogType.Warning, message);
+        }
+        public void Error(string message)
+        {
+            TypeLog(LogType.Error, message);
+        }
+    }
+}
