6fbe2d5663568ecc2624e0ccc03a2ef2c5d16bf0
Ivan Sh <irkrus3824@mail.ru>
2021-06-01 20:27:35 +0800

    Удаление ненужных классов

---
+++ Message_Types/Message_types.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Message_Types
+{
+    enum TypesMsg
+    {
+        Connection,Text
+    }
+    public class Msg
+    {
+        public Enum Type;
+        public string Name;
+        public DateTime Date;
+        public string Chat;
+    }
+    
+}
