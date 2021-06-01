42690155a7e93bab305f4bf55f816beda6a8d3c7
Ivan Sh <irkrus3824@mail.ru>
2021-06-01 19:56:57 +0800

    Добавление классов сообщений

---
+++ Message_Types/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Message_Types
+{
+    enum TypesMsg
+    {
+        ConDiscon,Text
+    }
+    public class Allmsg
+    {
+      protected string Name;
+      protected DateTime Date;
+      protected string Chat;
+    }
+
+    public class ConDiscon : Allmsg
+    {
+        public bool choice;
+    }
+
+    public class Text : Allmsg
+    {
+        public string msg;
+    }
+}
