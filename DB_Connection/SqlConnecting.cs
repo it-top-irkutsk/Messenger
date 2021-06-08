a025d903c9ba92b28c279f6a29d923caebb8474b
Anton <shtreehcode@gmail.com>
2021-06-08 18:52:20 +0800

    add all test methods, new readme docs

--- DB_Connection/SqlConnecting.cs
+++ DB_Connection/SqlConnecting.cs
@@ -1,15 +1,12 @@
 ﻿/*
  + CONNECTION - (Name || Data Source, Credentials, Optional parameters)
- TODO Selecting data from DB
- TODO Inserting data to DB
- TODO Updating data into DB
- TODO Deleting data from DB   
+ + Get data from DB
+ + Inserting data to DB
+ + Updating data in DB
+ + Deleting data from DB   
 */
 
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Data.SqlClient;
 
 namespace DB_Connection
@@ -24,16 +21,17 @@ namespace DB_Connection
         private string Catalog { get; init; }
         private string UserId { get; init; }
         public bool IsConnected { get; private set; }
-        public SqlConnecting(string dataSource, string catalog, string password, string userId)
+        public SqlConnecting(string dataSource, string catalog, string userId, string password)
         {
             IsConnected = false;
             DataSource = dataSource;
-            Password = password;
             Catalog = catalog;
             UserId = userId;
+            Password = password;
         }
         public void ConnectTo()
         {
+            if (IsConnected) throw new Exception();
             _connectionString = $"Data Source={DataSource};Initial Catalog={Catalog};User ID={UserId};Password={Password}";
             _cnn = new SqlConnection(_connectionString);
             _cnn.Open();
@@ -42,21 +40,21 @@ namespace DB_Connection
         
         public void DisconnectFrom()
         {
-            if (!IsConnected) return;
+            if (!IsConnected) throw new Exception();
             _connectionString = $"Data Source={DataSource};Initial Catalog={Catalog};User ID={UserId};Password={Password}";
             _cnn = new SqlConnection(_connectionString);
             _cnn.Close();
             IsConnected = false;
         }
 
-        public string GetDataFrom(string name)
+        public string GetDataFrom(string tableName)
         {
             if (!IsConnected) throw new Exception();
             SqlCommand command;
             SqlDataReader dataReader;
             String sqlString, output = "";
 
-            sqlString = $"SELECT * FROM {name};";
+            sqlString = $"SELECT * FROM {tableName};";
 
             command = new SqlCommand(sqlString, _cnn);
 
@@ -67,6 +65,57 @@ namespace DB_Connection
             }
 
             return output;
+        }
+
+        public void AddDataTo(string tableName, string columnsNames, string data)
+        {
+            if (!IsConnected) throw new Exception();
+            SqlCommand command;
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            String sqlString = "";
+
+            sqlString = $"INSERT INTO {tableName} ({columnsNames}) VALUES({data})";
+
+            command = new SqlCommand(sqlString, _cnn);
+
+            adapter.InsertCommand = new SqlCommand(sqlString, _cnn);
+            adapter.InsertCommand.ExecuteNonQuery();
+            
+            command.Dispose();
+        }
+
+        public void UpdateDataIn(string tableName, string updatingData, string id)
+        {
+            if (!IsConnected) throw new Exception();
+            SqlCommand command;
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            String sqlString = "";
+
+            sqlString = $"UPDATE {tableName} set {updatingData} where id={id}";
+            
+            command = new SqlCommand(sqlString, _cnn);
+            
+            adapter.InsertCommand = new SqlCommand(sqlString, _cnn);
+            adapter.InsertCommand.ExecuteNonQuery();
+            
+            command.Dispose();
+        }
+        
+        public void DeleteDataFrom(string tableName, string id)
+        {
+            if (!IsConnected) throw new Exception();
+            SqlCommand command;
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            String sqlString = "";
+
+            sqlString = $"DELETE {tableName} WHERE id={id}";
+            
+            command = new SqlCommand(sqlString, _cnn);
+            
+            adapter.InsertCommand = new SqlCommand(sqlString, _cnn);
+            adapter.InsertCommand.ExecuteNonQuery();
+            
+            command.Dispose();
         }
     }
 }
